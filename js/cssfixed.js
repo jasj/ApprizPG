dd7dd18b577804b10ddcefbab52a287ef16e0c0a
smartsoftint <asegreda@smartsoftint.com>
2015-03-11 21:20:05 -0600

    gaudy changes 2

--- js/cssfixed.js
+++ js/cssfixed.js
@@ -6,6 +6,7 @@ CSS fixing (due old android browsers)
 
 
 function fix_messages(){
+	alert(window.innerWidth);
 	$("#categories .centralLI").css({"width" : (window.innerWidth*2 +150)+"px"});
 	$("#categories .rightLI").css({"width" : (window.innerWidth +150)+"px"});
 	$("#categories .details").css({"width" : (window.innerWidth -20)+"px"});
