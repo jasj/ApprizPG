201548f103c12e12997b40d4032b47c7c477d90e
smartsoftint <asegreda@smartsoftint.com>
2015-03-19 12:31:17 -0600

    nam

--- js/cssfixed.js
+++ js/cssfixed.js
@@ -7,13 +7,21 @@ CSS fixing (due old android browsers)
 
 function fix_messages(){
 	//alert(window.innerWidth);
+
 	$("#categories .moveContainer").css({"left" : (0-window.innerWidth -21)+"px"});
 	$("#categories .centralLI").css({"width" : (window.innerWidth*2 +150)+"px"});
 	$("#categories .centralLI").css({"left" : (window.innerWidth +20)+"px"});
 	$("#categories .rightLI").css({"width" : (window.innerWidth +150)+"px"});
 	$("#categories .details").css({"width" : (window.innerWidth -20)+"px"});
-	$("#categories li .infoBank").css({"width" : (window.innerWidth -160)+"px"});
-	$(".dateBank").css({"width" : (window.innerWidth -160)+"px"});
+    if (window.innerWidth > 325) {
+        $("#categories li .infoBank").css({"width": (window.innerWidth - 160) + "px"});
+        $(".dateBank").css({"width": (window.innerWidth - 160) + "px"});
+    }else{
+        $("#categories li .infoBank").css({"width": "190px"});
+    //    $(".dateBank").css({"width":  "190px"});
+
+
+    }
 		
 }
 
