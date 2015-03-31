2d4336e50ad5bca919559b815e97e27ec4bcc3f8
smartsoftint <asegreda@smartsoftint.com>
2015-03-31 04:01:25 -0600

    files 1

--- js/files.js
+++ js/files.js
@@ -1,42 +1,20 @@
-window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
-var fs = null;
+ document.addEventListener("deviceready", onDeviceReady, false);
 
-function errorHandler(e) {
-  var msg = '';
-  switch (e.code) {
-    case FileError.QUOTA_EXCEEDED_ERR:
-      msg = 'QUOTA_EXCEEDED_ERR';
-      break;
-    case FileError.NOT_FOUND_ERR:
-      msg = 'NOT_FOUND_ERR';
-      break;
-    case FileError.SECURITY_ERR:
-      msg = 'SECURITY_ERR';
-      break;
-    case FileError.INVALID_MODIFICATION_ERR:
-      msg = 'INVALID_MODIFICATION_ERR';
-      break;
-    case FileError.INVALID_STATE_ERR:
-      msg = 'INVALID_STATE_ERR';
-      break;
-    default:
-      msg = 'Unknown Error';
-      break;
-  };
-  alert('Error: ' + msg);
-}
+    // device APIs are available
+    //
+    function onDeviceReady() {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onFileSystemSuccess, fail);
+        window.resolveLocalFileSystemURI("file:///example.txt", onResolveSuccess, fail);
+    }
 
-function initFS() {
-  window.requestFileSystem(window.PERSISTENT , 1024*1024, function(filesystem) {
-    fs = filesystem;
-	alert("sinc")
-  }, errorHandler);
-}
+    function onFileSystemSuccess(fileSystem) {
+        console.log(fileSystem.name);
+    }
 
+    function onResolveSuccess(fileEntry) {
+        console.log(fileEntry.name);
+    }
 
-// Initiate filesystem on page load.
-if (window.requestFileSystem) {
-  initFS();
-}else{
-	console.log("requestFileSystem nop");
-}
+    function fail(error) {
+        console.log(error.code);
+    }
