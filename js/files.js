b17205b6c859871b7506749577a638965e6154df
smartsoftint <asegreda@smartsoftint.com>
2015-03-31 22:10:21 -0600

    auto

--- js/files.js
+++ js/files.js
@@ -1,3 +1,31 @@
+ function errorHandler(e) {
+  var msg = '';
+
+  switch (e.code) {
+    case FileError.QUOTA_EXCEEDED_ERR:
+      msg = 'QUOTA_EXCEEDED_ERR';
+      break;
+    case FileError.NOT_FOUND_ERR:
+      msg = 'NOT_FOUND_ERR';
+      break;
+    case FileError.SECURITY_ERR:
+      msg = 'SECURITY_ERR';
+      break;
+    case FileError.INVALID_MODIFICATION_ERR:
+      msg = 'INVALID_MODIFICATION_ERR';
+      break;
+    case FileError.INVALID_STATE_ERR:
+      msg = 'INVALID_STATE_ERR';
+      break;
+    default:
+      msg = 'Unknown Error';
+      break;
+  };
+
+  alert('Error: ' + msg);
+}
+
+ 
  // Wait for device API libraries to load
     //
     document.addEventListener("deviceready", onDeviceReady, false);
@@ -35,9 +63,10 @@
 
 
     function gotFileWriter(writer) {
-		
+		alert(1);
 	$.get("https://s.yimg.com/zz/combo?pv/static/lib/srp-core-css-purple_3acecc2e83faa9ba766268df3f1c20c1.css",function(data){
 		 writer.write(data);
+		 alert(data);
 	  });
        
     }
