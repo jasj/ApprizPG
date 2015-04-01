47051752f7c4491db122bc9f01d7569909519219
smartsoftint <asegreda@smartsoftint.com>
2015-03-31 23:42:14 -0600

    auto

--- js/entity.js
+++ js/entity.js
@@ -48,7 +48,7 @@ function loadEntityTemplate(){
 	$('.bankBrand img').attr("alt", "");
 	$('.bankBrand img').attr("src", IMG);
 	getFileLocalURL(FormatInteger(currentEntityID,4)+".css", $('#entityStyle'), "href");
-	$('#entityStyle').attr("href",S3Bucket+FormatInteger(currentEntityID,4)+'/CSS/entity.css');
+	//$('#entityStyle').attr("href",S3Bucket+FormatInteger(currentEntityID,4)+'/CSS/entity.css');
 	tabSelectedColor = $("#colorHandle").css("color");//"rgb(85, 185, 73)"
 }
 		
