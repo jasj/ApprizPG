544cd17b3b35f0177c86ba64f7c36fb60f502670
smartsoftint <asegreda@smartsoftint.com>
2015-04-04 18:47:15 -0600

    home 4of4

--- js/start.js
+++ js/start.js
@@ -9,9 +9,7 @@ function onDeviceReady(){
 
 if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
   document.addEventListener("deviceready", onDeviceReady, false);
-  alert(12);
 } else {
-	alert(1);
   onDeviceReady(); //this is the browser
 }
 
