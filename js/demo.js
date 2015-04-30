8a6d9376721361327b939c55ba562a8233201496
smartsoftint <asegreda@smartsoftint.com>
2015-04-30 11:47:02 -0600

    iphone back msh v1 and demo v2

--- js/demo.js
+++ js/demo.js
@@ -1,4 +1,4 @@
-$("#demo button").tapend(function(){
+$("#demo[header=headerGeneral] button").tapend(function(){
 	requestService({"productName": "My Debit Card-5412","id" : "CMT0028" ,"code": "DEMO-en", "description": "DEMO","entityName" : $('#entities li[entityId='+currentEntityID+']').find('img').attr("alt"),"demo" : "demo"});
 	
 });
