effc4e14a403e5f0969a69fa0cd45e47b1d051c3
JoseRaulAlfaro <joseraulalfaroguillen@gmail.com>
2015-06-05 11:44:09 -0600

    demo-

--- js/demo.js
+++ js/demo.js
@@ -1,5 +1,5 @@
 $("#demo[header=headerGeneral] button").tapend(function(){
 	console.log("Sending Demo Request");
-	requestService({"productName": "Ahorro-250","code": "DEMO-en", "description": "DEMO","entityName" : $('#entities li[entityId='+currentEntityID+']').find('img').attr("alt"),"demo" : "demo"});
+	requestService({"productName": "My Amex","code": "DEMO-en", "description": "DEMO","entityName" : $('#entities li[entityId='+currentEntityID+']').find('img').attr("alt"),"demo" : "demo"});
 	
 });
