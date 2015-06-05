5684d78afa28f1b0a47c57f561ad03ee8092b736
JoseRaulAlfaro <joseraulalfaroguillen@gmail.com>
2015-06-05 14:02:26 -0600

    -

--- js/main.js
+++ js/main.js
@@ -181,7 +181,7 @@ function menuKeyDown(){
 function onResume(){
 
 	if(currentEntityID > 0){
-	
+	console.log("-");
 		try{
 			pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, $('.unread').length);
 		}catch(e){
